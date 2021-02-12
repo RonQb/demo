f41ae0e0dbf1cbbc1fabd46c34631d927ef87f83
RonQb <quanxiaofei1234@163.com>
2021-02-12 17:59:57 +0800

    feat：test

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+   @RequestMapping("/test1")
+    public String hello(){
+        return "Hello Spring Boot";
+    }
+
+
+    @RequestMapping("/test2")
+    public String hello2(){
+
+       System.out.println("ddd");return "Hello Spring Boot111";
+
+    }
+}
